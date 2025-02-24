8087c82a9e16319aa04e7cc77f8b143ef8b3a6f4
tahir826 <mtahirhassan826@gmail.com>
2025-02-25 00:05:53 +0500

    initial commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
+
+# Initialize the Gemini model
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=os.getenv("GOOGLE_API_KEY"))
+
+
+while True:
+    userInput = input("define query :- ")
+    result = llm.invoke(userInput)
+    print(result.content)
+    if userInput=="exit":
+        break
+
+# Generate a response
+# result = llm.invoke("hi")
+# print(result.content)
