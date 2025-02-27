d3c677e9a8e2d63546fca6226877f374a1657b91
tahir826 <mtahirhassan826@gmail.com>
2025-02-27 10:03:01 +0500

    add memory concept

--- hello.py
+++ hello.py
@@ -1,21 +1,73 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from dotenv import load_dotenv
+from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory, ConversationSummaryMemory, ConversationSummaryBufferMemory
+from langchain.chains import ConversationChain
+from dotenv import load_dotenv  
 import os
-
-# Load environment variables
 load_dotenv()
 
-# Initialize the Gemini model
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=os.getenv("GOOGLE_API_KEY"))
+llm1 = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash", google_api_key=os.getenv("GOOGLE_API_KEY"))
+
+memory1 = ConversationBufferMemory()
+memory2 = ConversationBufferWindowMemory(k = 2)
+memory3 = ConversationSummaryMemory(llm=llm)
+memory4 = ConversationSummaryBufferMemory(llm=llm, max_token_limit=1000)
+
+# Put here the memory which one you want to use
+chain = ConversationChain(llm=llm1, memory=memory3)
+
+while True: 
+    user_input = input("You: ")
+    if user_input == "exit":
+        break
+    response = chain.invoke(user_input)
+    print("Final==>>",response)
 
 
-# while True:
-#     userInput = input("define query :- ")
-#     result = llm.invoke(userInput)
-#     print(result.content)
-#     if userInput=="exit":
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory,ConversationSummaryMemory, ConversationSummaryBufferMemory
+# from langchain.chains import ConversationChain
+# from dotenv import load_dotenv
+# import os
+# load_dotenv()
+
+# llm = ChatGoogleGenerativeAI(
+#     model="gemini-1.5-flash", google_api_key=os.getenv("GOOGLE_API_KEY"))
+
+
+# memory = ConversationBufferMemory()
+
+
+# chain = ConversationChain(llm=llm, memory=memory)
+
+# while True: 
+#     user_input = input("You: ")
+#     if user_input == "exit":
 #         break
-
-# Generate a response
-# result = llm.invoke("hi")
-# print(result.content)
+#     response = chain.invoke(user_input)
+#     print("Final==>>",response)
