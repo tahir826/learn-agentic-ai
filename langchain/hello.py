ecedbf86a353a591033a44a2b9c5bff2cf6cf68f
tahir826 <mtahirhassan@gmail.com>
2025-03-02 20:40:16 +0500

    .

--- langchain/hello.py
+++ langchain/hello.py
@@ -1,10 +1,10 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import OpenAI
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
-llm1 = ChatGoogleGenerativeAI(
-    model="gemini-2.0-flash", google_api_key=os.getenv("GOOGLE_API_KEY"))
+llm1 = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"))
 
 response = llm1.invoke("hi")
 print(response)
